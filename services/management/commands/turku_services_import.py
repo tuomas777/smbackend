17bb26d172f486af68f430de2cba1bd1dddaaeff
Frank Wickström <frank.wickstrom@anders.fi>
2018-05-07 12:08:08 +0300

    Add base turku importer

---
+++ services/management/commands/turku_services_import.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import logging
+import sys
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils import translation
+
+
+class Command(BaseCommand):
+    help = "Import services from City of Turku APIs"
+    importer_types = []
+    supported_languages = [l[0] for l in settings.LANGUAGES]
+
+    def __init__(self):
+        super(Command, self).__init__()
+        for imp in self.importer_types:
+            method = "import_%s" % imp
+            assert getattr(self, method, False), "No importer defined for %s" % method
+
+        self.services = {}
+
+        self.options = None
+        self.verbosity = 1
+        self.logger = None
+
+    def add_arguments(self, parser):
+        parser.add_argument('import_types', nargs='*', choices=self.importer_types)
+        parser.add_argument('--cached', action='store_true', dest='cached',
+                            default=False, help='cache HTTP requests')
+        parser.add_argument('--single', action='store', dest='id',
+                            default=False, help='import only single entity')
+
+    # Activate the default language for the duration of the import
+    # to make sure translated fields are populated correctly.
+    @translation.override(settings.LANGUAGES[0][0])
+    def handle(self, **options):
+        self.options = options
+        self.verbosity = int(options.get('verbosity', 1))
+        self.logger = logging.getLogger(__name__)
+
+        import_count = 0
+        for imp in self.importer_types:
+            if imp not in self.options["import_types"]:
+                continue
+            method = getattr(self, "import_%s" % imp)
+            if self.verbosity:
+                print("Importing %s..." % imp)
+            method()
+            import_count += 1
+
+        if not import_count:
+            sys.stderr.write("Nothing to import.\n")
